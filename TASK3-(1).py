c399e9bf4973a29f0f7e753f29a8d0ceca924870
harsha varthan <66173033+rpharshavarthan@users.noreply.github.com>
2020-08-31 21:04:47 +0530

    Add files via upload

--- TASK3-(1).py
+++ TASK3-(1).py
@@ -1,15 +1,17 @@
 # TASK3-(1)
-
-digit = list(map(int, input()))
+n = input()
+digit = list(map(int, n))
 sum_oprn = 0
-while True:
-    digit_sum = sum(digit)
-    digit = list(map(int, str(digit_sum)))
-    sum_oprn += 1
-    if digit_sum < 10:
-        break
-
-print(sum_oprn)
+if int(n) < 10:
+    print(sum_oprn)
+else:
+    while True:
+        digit_sum = sum(digit)
+        digit = list(map(int, str(digit_sum)))
+        sum_oprn += 1
+        if digit_sum < 10:
+            break
+    print(sum_oprn)
 
 
 
